bd30a4aab3e8f6bc273b5b92dc0d4a440a43c15a
Kilian Koeltzsch <me@kilian.io>
2017-01-11 23:11:31 +0100

    Set DateFormatter timezone to UTC
    fixes #2

--- Sources/DateFormatter+format.swift
+++ Sources/DateFormatter+format.swift
@@ -12,6 +12,7 @@ internal extension DateFormatter {
     static func iso() -> DateFormatter {
         let df = DateFormatter()
         df.dateFormat = "yyyy-MM-dd'T'HH:mm:ss"
+        df.timeZone = TimeZone(identifier: "UTC")
         return df
     }
 }
