23196d2b3b2f4b5ae1cc867f025bdeb2568397eb
Kilian Koeltzsch <me@kilian.io>
2019-08-19 22:42:09 +0200

    Specify supported platforms in manifest

--- Package.swift
+++ Package.swift
@@ -3,6 +3,12 @@ import PackageDescription
 
 let package = Package(
     name: "ParkKit",
+    platforms: [
+        .macOS(.v10_12),
+        .iOS(.v10),
+        .watchOS(.v3),
+        .tvOS(.v10)
+    ],
     products: [
         .library(
             name: "ParkKit",
